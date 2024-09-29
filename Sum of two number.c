d3dad3af5c8edd561e4a282218b3a53afa8c2de5
Md. Shariar Bin Alam <abir3935@gmail.com>
2024-09-29 21:46:46 +0600

    Add files via upload

---
+++ Sum of two number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+
+{ int a,b,c;
+printf("Enter two number for sum :\n");
+printf("1st Number:");
+scanf("%d",&a);
+printf("2st Number:");
+scanf("%d",&b);
+c=a+b;
+printf("Answer=%d",c);
+
+return 0;
+
+}
