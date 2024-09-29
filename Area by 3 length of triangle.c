62bf03d23fa6393cf5531b5932367dbc22b64e26
Md. Shariar Bin Alam <abir3935@gmail.com>
2024-09-29 22:00:50 +0600

    Add files via upload
    By this code we can calculate the area of a triangle by simply input the 3 line of length

---
+++ Area by 3 length of triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<cmath>
+
+int main()
+ {
+
+    float a, b, c, s, area;
+
+
+    printf ("Enter the length of side a: ");
+    scanf  ("%f",&a);
+
+    printf ("Enter the length of side b: ");
+    scanf  ("%f",&b);
+
+    printf ("Enter the length of side c: ");
+    scanf("%f",&c);
+
+
+    s = (a + b + c) / 2;
+
+
+    area = sqrt (s * (s - a) * (s - b) * (s - c));
+
+
+    printf("The area of the triangle is: %f",area);
+
+    return 0;
+}
+
