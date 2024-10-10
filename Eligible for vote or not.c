a72037f5b528d0e1ddb192535f73b40a242a2eb3
Md. Shariar Bin Alam <abir3935@gmail.com>
2024-10-10 22:39:00 +0600

    Add files via upload

---
+++ Eligible for vote or not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ int main()
+
+ {
+     int age;
+     printf("Enter the age: ");
+     scanf("%d",&age);
+
+     if (age >= 18)
+
+     {
+         printf("You are eligible for voting.");
+     }
+     else
+     {
+         printf("You are not eligible for voting.");
+     }
+     return 0;
+ }
