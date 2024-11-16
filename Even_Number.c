2e6fac35f354f6dd5dabd391c96f0b225df48583
Md. Shariar Bin Alam <abir3935@gmail.com>
2024-11-16 23:25:34 +0600

    Add files via upload

---
+++ Even_Number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int main() {
+
+     int n, i;
+
+     scanf("%d", &n);
+
+     if (n < 2) {
+        printf("-1\n");
+     } else {
+
+     for (i = 2; i <= n; i += 2)
+
+          {
+              printf("%d\n", i);
+          }
+    }
+
+    return 0;
+ }
