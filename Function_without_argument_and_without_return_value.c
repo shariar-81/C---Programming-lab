f4d9db0925ba1bb1bd61e56ca7705d06e9a717e5
Md. Shariar Bin Alam <abir3935@gmail.com>
2025-03-02 21:44:39 +0600

    Add files via upload

---
+++ Function_without_argument_and_without_return_value.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void print1();
+void print2();
+void print3();
+
+
+int main () {
+
+    printf("Hello \n");
+
+    print1();
+    print2();
+    print3();
+
+return 0;
+
+}
+
+void print1() {
+
+     printf("Welcome to C programming with Abir ");
+
+}
+
+void print2() {
+
+     printf("HA HA \n");
+
+}
+
+void print3() {
+
+     printf("Meow");
+}
+
